eed9655950c24afcdbb154c2cb5d99d346f0a904
Joao Silva <joao.ms94@gmail.com>
2019-02-01 10:23:24 -0100

    Fix builds having wrong architecture description.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 rm -rf "./bin"
 mkdir "./bin"
 
-env GOOS=linux GOARCH=amd64 go build -o ./bin/packtpub-linux-arm64 main.go
-env GOOS=darwin GOARCH=amd64 go build -o ./bin/packtpub-macos-arm64 main.go
-env GOOS=windows GOARCH=amd64 go build -o ./bin/packtpub-windows-arm64.exe main.go
+env GOOS=linux GOARCH=amd64 go build -o ./bin/packtpub-linux-amd64 main.go
+env GOOS=darwin GOARCH=amd64 go build -o ./bin/packtpub-macos-amd64 main.go
+env GOOS=windows GOARCH=amd64 go build -o ./bin/packtpub-windows-amd64.exe main.go
